bd9e5d008508b3615c6620fb18afc4fb89f8477d
LinzhouLi <linzhouli@outlook.com>
2024-03-03 22:34:01 +0800

    phong material

--- include/pt/material.h
+++ include/pt/material.h
@@ -44,4 +44,45 @@ private:
 	Bitmap* m_diffuse_texture = nullptr;
 };
 
+
+float sampleCosineWeightedHemisphere(const Vector2f& u) {
+
+}
+
+
+class PhongBRDF {
+public:
+	PhongBRDF(const Vector3f& diffuse, const Vector3f& specular, float shininess) {
+		m_Ns = shininess;
+		m_Kd = diffuse;
+		m_Ks_coef = (m_Ns + 2.0) * INV_TWOPI;
+		m_Ks = specular / m_Ks_coef;
+		m_p = m_Kd.maxCoeff() / (m_Kd.maxCoeff() + m_Ks.maxCoeff());
+	}
+
+	Vector3f f(const Vector3f& wo, const Vector3f& wi) {
+		// SameHemisphere
+		return m_Kd * INV_PI + m_Ks * m_Ks_coef * std::powf(wo.dot(wi), m_Ns);
+	}
+
+	Vector3f sample_f(const Vector3f& wo, float uc, const Vector2f& u) {
+
+	}
+
+	float pdf() {
+
+	}
+
+private:
+	Vector3f sampleSpecularLobe(const Vector2f& u) {
+		float theta = 2.0f * M_PI * u.y();
+		float cos_alpha = std::powf(u.x(), 1.0f / (m_Ns + 1.0f));
+		float sin_alpha = std::sqrt(1.0f - cos_alpha * cos_alpha);
+		return Vector3f(sin_alpha * std::cos(theta), sin_alpha * std::cosf(theta), cos_alpha);
+	}
+
+	Vector3f m_Kd, m_Ks;
+	float m_Ns, m_p, m_Ks_coef;
+};
+
 }
